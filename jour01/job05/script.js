030ca09b97b1842318ee59c35daa754d8fcf1299
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-20 11:33:32 +0200

    feat : complete jobs 05 and 06

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+function displayDaysOfTheWeek() {
+
+    const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]; 
+
+    for (day of DAYS_OF_THE_WEEK) {
+        console.log(day);
+    }
+}
+
+displayDaysOfTheWeek();
