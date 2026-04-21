fe6bc991a2425f804e641bee45657a481b72ee8a
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-21 09:54:34 +0200

    feat : complete day 2 job 1

---
+++ jour02/job01/script.js
@@ -0,0 +1,8 @@
+function printTheCitationInConsole() {
+
+    // Select the citation article by its ID
+    let myCitation = document.getElementById("citation");
+    // .textContent to get its text content
+    console.log(myCitation.textContent);
+
+}
