bba5f4d084a1a239403c24dcfa5fe257d6a14650
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-21 10:58:58 +0200

    feat : complete exercice 4

---
+++ jour02/job04/script.js
@@ -0,0 +1,17 @@
+function keyListener() { 
+
+    const keyLogger = document.getElementById("keylogger");
+
+    // chaine regex, ^ début, $ fin, i case insensitive, .test méthode
+    // d'un objet regex qui renvoie true si l'argument correspond
+    if (/^[a-z]$/i.test(event.key)) {
+
+        keyLogger.value += event.key
+
+    }
+
+}
+
+// écoute toujours, donc rajoute 1 caractère au comportement normal de la textarea
+// 0+1 = 1, 1+1 = 2 
+document.addEventListener("keydown", keyListener);
