8c790a5d35ab788f02719588abdc7841dbc3a3cd
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-22 15:02:00 +0200

    fix : animation now stops when focus/blur condition ends

--- jour03/job04/script.js
+++ jour03/job04/script.js
@@ -1,12 +1,15 @@
-$(document).ready(function(){
-  
-    $("#input-zone").focus(function() {
-        $(this).animate({width: '+=1300px'},8000)
+$(document).ready(function () {
+
+    $("#input-zone").focus(function () {
+        $(this)
+            .stop(true, false)              // stop animation in progress
+            .animate({ width: "500px" }, 6000); // expand
     });
 
-    $("#input-zone").blur(function() {
-        $(this).animate({width: '-=1300px'},8000)
+    $("#input-zone").blur(function () {
+        $(this)
+            .stop(true, false)              // stop animation in progress
+            .animate({ width: "100px" }, 6000); // shrink back
     });
 
 });
-
